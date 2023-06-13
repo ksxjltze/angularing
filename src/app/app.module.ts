143cc4eae8da5f520705ac0d231579a102233fd1
Lee Jia Keat <leejiakeat@gmail.com>
2023-06-13 23:02:48 +0800

    more angularing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, provideProtractorTestingSupport } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { RouterModule, provideRouter } from '@angular/router';
+import routeConfig from './routes'
 
 @NgModule({
   declarations: [
@@ -12,9 +14,13 @@ import { HomeComponent } from './home/home.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    RouterModule,
     HomeComponent
   ],
-  providers: [],
+  providers: [
+    provideProtractorTestingSupport(),
+    provideRouter(routeConfig)
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { HousingLocation } from '../housinglocation';
+import { HousingService } from '../housing.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    HousingLocationComponent,
+  ],
   template: `
   <section>
     <form>
@@ -12,9 +18,21 @@ import { CommonModule } from '@angular/common';
       <button class="primary" type="button">Search</button>
     </form>
   </section>
+  <section class="results">
+    <app-housing-location
+      *ngFor="let housingLocation of housingLocationList"
+      [housingLocation]="housingLocation">
+    </app-housing-location>
+  </section>
 `,
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  housingLocationList: HousingLocation[] = [];
+  housingService: HousingService = inject(HousingService);
+
+  constructor() {
+    this.housingLocationList = this.housingService.getAllHousingLocations();
+  }
 
 }
