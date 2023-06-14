dc2d29e7d97fa45c4b0e98e57b37916610e8f43a
Lee Jia Keat <leejiakeat@gmail.com>
2023-06-14 22:16:15 +0800

    filter thingy

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,6 +4,12 @@ import { HousingLocationComponent } from '../housing-location/housing-location.c
 import { HousingLocation } from '../housinglocation';
 import { HousingService } from '../housing.service';
 
+enum FilterType{
+    City = "CITY",
+    State = "STATE",
+    Name = "NAME"
+}
+
 @Component({
   selector: 'app-home',
   standalone: true,
@@ -13,14 +19,23 @@ import { HousingService } from '../housing.service';
   ],
   template: `
   <section>
+    <div id="filter-types">
+      <label for="filter-types" id="filter-types-label">Filter Types:</label>
+      <select id="filter-types-select" #filter_type (change)="filterSelected(filter_type.value)">
+        <option value="City">City</option>
+        <option value="State">State</option>
+        <option value="Name">Name</option>
+      </select>
+    </div>
     <form>
-      <input type="text" placeholder="Filter by city">
-      <button class="primary" type="button">Search</button>
+      <input type="text" placeholder="Filter" #filter>
+      <button class="primary" type="button" (click)="filterResults(filter.value)">Search</button>
     </form>
   </section>
+
   <section class="results">
     <app-housing-location
-      *ngFor="let housingLocation of housingLocationList"
+      *ngFor="let housingLocation of filteredLocationList"
       [housingLocation]="housingLocation">
     </app-housing-location>
   </section>
@@ -29,10 +44,36 @@ import { HousingService } from '../housing.service';
 })
 export class HomeComponent {
   housingLocationList: HousingLocation[] = [];
+  filteredLocationList: HousingLocation[] = []; 
   housingService: HousingService = inject(HousingService);
+
+  filterType: string = "City";
 
   constructor() {
     this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.filteredLocationList = this.housingLocationList;
+  }
+
+  filterResults(text: string){
+    if (!text){
+      this.filteredLocationList = this.housingLocationList;
+    }
+
+    switch(this.filterType){
+      case "City":
+        this.filteredLocationList = this.housingLocationList.filter(housingLocation => housingLocation?.city.toLowerCase().includes(text.toLowerCase()));
+        break;
+      case "State":
+        this.filteredLocationList = this.housingLocationList.filter(housingLocation => housingLocation?.state.toLowerCase().includes(text.toLowerCase()));
+        break;
+      case "Name":
+        this.filteredLocationList = this.housingLocationList.filter(housingLocation => housingLocation?.name.toLowerCase().includes(text.toLowerCase()));
+        break;
+    }
+  }
+
+  filterSelected(filter: string){
+    this.filterType = filter;
   }
 
 }
